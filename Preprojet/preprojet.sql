b910cfdc906e3563c04b690abbce73b7d71fd217
Jonathan CASIER <jonathancasier@gmail.com>
2022-10-24 21:38:40 +0200

    Q8 : OK

--- Preprojet/preprojet.sql
+++ Preprojet/preprojet.sql
@@ -2,7 +2,6 @@ DROP SCHEMA IF EXISTS preprojet CASCADE;
 CREATE SCHEMA preprojet;
 
 -- QUESTION 2
-
 CREATE TABLE preprojet.personnes
 (
     id_personne SERIAL PRIMARY KEY,
@@ -19,15 +18,14 @@ CREATE TABLE preprojet.comptes
 CREATE TABLE preprojet.operations
 (
     id_operation       SERIAL PRIMARY KEY,
-    compte_source      CHARACTER(10) REFERENCES preprojet.comptes (numero) NOT NULL,
-    compte_destination CHARACTER(10) REFERENCES preprojet.comptes (numero) NOT NULL,
+    compte_source      CHARACTER(9) REFERENCES preprojet.comptes (numero) NOT NULL,
+    compte_destination CHARACTER(9) REFERENCES preprojet.comptes (numero) NOT NULL,
     montant            INTEGER                                             NOT NULL CHECK (montant > 0),
     date_op            TIMESTAMP                                           NOT NULL,
     CHECK (compte_source <> compte_destination)
 );
 
 -- QUESTION 3
-
 INSERT INTO preprojet.personnes VALUES (DEFAULT, 'Damas', 'Christophe');
 INSERT INTO preprojet.personnes VALUES (DEFAULT, 'Cambron', 'Isabelle');
 INSERT INTO preprojet.personnes VALUES (DEFAULT, 'Ferneeuw', 'Stephanie');
@@ -48,8 +46,7 @@ INSERT INTO preprojet.operations VALUES (DEFAULT, 123456789, 563212564, 100, '20
 INSERT INTO preprojet.operations VALUES (DEFAULT, 987687654, 789623565, 80, '2006-12-08');
 INSERT INTO preprojet.operations VALUES (DEFAULT, 789623565, 987687654, 80, '2006-12-09');
 
--- QUESTION 4
-
+-- QUESTION 4 : ok
 select distinct pers_src.nom as "Nom source", pers_src.prenom as "Prenom source", cpt_src.numero as "Compte source",
                 pers_dest.nom as "Nom destination", pers_dest.prenom as "Prenom destination", cpt_dest.numero as "Compte destination",
                 o.date_op as "Date operation", o.montant as "Montant"
@@ -59,7 +56,6 @@ select distinct pers_src.nom as "Nom source", pers_src.prenom as "Prenom source"
     order by  o.date_op;
 
 -- QUESTION 5
-
 CREATE OR REPLACE FUNCTION preprojet.insererOperation(nom_source VARCHAR(100), prenom_source VARCHAR(100), compte_source CHARACTER(9),
     nom_destination VARCHAR(100), prenom_destination VARCHAR(100), compte_destination CHARACTER(9),
     date_operation TIMESTAMP, montant_operation INTEGER) RETURNS INTEGER AS $$
@@ -87,72 +83,120 @@ CREATE OR REPLACE FUNCTION preprojet.insererOperation(nom_source VARCHAR(100), p
         RETURN id;
     END;
 $$ LANGUAGE plpgsql;
--- QUESTION 5
+-- QUESTION 5 : ok
 select * from preprojet.insererOperation(VARCHAR(100) 'Damas', VARCHAR(100) 'Christophe',
     CHARACTER(9) '123456789', VARCHAR(100) 'Ferneeuw', VARCHAR(100) 'Stephanie',
     CHARACTER(9) '789623565', TIMESTAMP '2022-10-22', 20);
 
 -- QUESTION 6
-
 CREATE OR REPLACE FUNCTION preprojet.modifierMontantOperation(nom_source VARCHAR(100), prenom_source VARCHAR(100),
     cpt_source CHARACTER(9), nom_destination VARCHAR(100), prenom_destination VARCHAR(100), cpt_destination CHARACTER(9),
-    date_operation TIMESTAMP, montant_operation INTEGER) RETURNS INTEGER AS $$
+    date_operation TIMESTAMP, montant_operation INTEGER) RETURNS RECORD AS $$
     DECLARE
-        operation RECORD;
+        ret RECORD;
     BEGIN
-        /*
-        IF((select count(o.id_operation)
-            from preprojet.comptes cpt_src, preprojet.comptes cpt_dest, preprojet.operations o
-            where o.compte_source = cpt_source and o.compte_destination = cpt_destination
+
+        IF((SELECT count(DISTINCT o.*)
+            FROM preprojet.comptes cpt_src, preprojet.comptes cpt_dest, preprojet.operations o
+            WHERE o.compte_source = cpt_source AND o.compte_destination = cpt_destination AND o.date_op = date_operation
             ) <> 1)
         THEN
             RAISE foreign_key_violation;
         ELSE
-         */
-            select distinct o.*
-            from preprojet.comptes cpt_src, preprojet.comptes cpt_dest, preprojet.operations o
-            where o.compte_source = cpt_source and o.compte_destination = cpt_destination and o.date_op = date_operation
-            INTO operation;
-            UPDATE preprojet.operations op SET montant=montant_operation where op.id_operation = operation.id_operation;
-        --END IF;
+            SELECT DISTINCT o.*
+            INTO ret
+            FROM preprojet.comptes cpt_src, preprojet.comptes cpt_dest, preprojet.operations o
+            WHERE o.compte_source = cpt_source
+              AND o.compte_destination = cpt_destination
+              AND o.date_op = date_operation;
+
+            UPDATE preprojet.operations op SET montant=montant_operation WHERE op.id_operation = ret.id_operation;
+
+            RETURN ret;
+        END IF;
     END;
-$$ LANGUAGE  plpgsql;
-
--- verif update : ok
---UPDATE preprojet.operations op SET montant='20' where op.id_operation = 1;
--- verif timestamp in select : ok
---select * from preprojet.operations where date_op = '2006-12-01';
--- verif select question 6 : ok
---select distinct o.*
---from preprojet.comptes cpt_src, preprojet.comptes cpt_dest, preprojet.operations o
---where o.compte_source = '123456789' and o.compte_destination = '563212564' and o.date_op = '2006-12-01';
-
+$$ LANGUAGE plpgsql;
 -- QUESTION 6
 SELECT * FROM preprojet.modifierMontantOperation(VARCHAR(100) 'Damas',VARCHAR(100) 'Christophe',
     CHARACTER(9) '123456789',VARCHAR(100) 'Cambron',VARCHAR(100) 'Isabelle',
-    CHARACTER(9) '563212564', TIMESTAMP '2006-12-01', 10);
+    CHARACTER(9) '563212564', TIMESTAMP '2006-12-01', 20)
+    t(id int, src character(9), dest character(9), montant int, date timestamp);
+-- Question 6 : ok
+select * from preprojet.operations o where o.id_operation = 1;
 
 -- QUESTION 7
-
-
+CREATE OR REPLACE FUNCTION preprojet.supprimerOperation(nom_source VARCHAR(100), prenom_source VARCHAR(100),
+    cpt_source CHARACTER(9), nom_destination VARCHAR(100), prenom_destination VARCHAR(100), cpt_destination CHARACTER(9),
+    date_operation TIMESTAMP, montant_operation INTEGER) RETURNS BOOLEAN AS $$
+    DECLARE
+        ret BOOLEAN:=false;
+        record RECORD;
+    BEGIN
+        FOR record IN SELECT * FROM preprojet.operations LOOP
+            IF record.compte_source = cpt_source
+                AND record.compte_destination = cpt_destination
+                AND record.date_op = date_operation
+                AND record.montant = montant_operation
+            THEN
+                DELETE
+                    FROM preprojet.operations o
+                    WHERE record.compte_source = o.compte_source
+                        AND record.compte_destination = o.compte_destination
+                        AND record.date_op = o.date_op
+                        AND record.montant = o.montant;
+                ret = true;
+            END IF;
+        END LOOP;
+        return ret;
+    END;
+$$ LANGUAGE plpgsql;
+-- Question 7
+--select * from preprojet.supprimerOperation(VARCHAR(100) 'Damas',VARCHAR(100) 'Christophe',
+--    CHARACTER(9) '123456789',VARCHAR(100) 'Cambron',VARCHAR(100) 'Isabelle',
+--    CHARACTER(9) '563212564', TIMESTAMP '2006-12-01', 20);
+-- Question 7 : ok
+select * from preprojet.operations;
 
 -- QUESTION 8
-
-
+/*
+    Créez une procédure qui affiche l’évolution d’un compte bancaire au cours du temps.
+    Le paramètre de la procédure est le numéro du compte bancaire. A chaque fois qu’il y a une opération avec ce compte,
+    une ligne affiche la date de l’opération, avec qui cette opération se fait et quelle est la balance du compte suite
+    à cette dernière.
+ */
+CREATE OR REPLACE FUNCTION preprojet.balance(compte CHARACTER(9)) RETURNS SETOF RECORD AS $$
+    DECLARE
+        record RECORD;
+        rec RECORD;
+        balance INTEGER:=0;
+    BEGIN
+        FOR record IN SELECT o.date_op, o.compte_source, o.compte_destination, o.montant FROM preprojet.operations o LOOP
+            IF compte = record.compte_source THEN
+                balance := balance-record.montant;
+                SELECT record.date_op, record.compte_destination, balance INTO rec;
+                RETURN NEXT rec;
+            ELSEIF compte = record.compte_destination THEN
+                balance := balance+record.montant;
+                SELECT record.date_op, record.compte_source, balance INTO rec;
+                RETURN NEXT rec;
+            END IF;
+        END LOOP;
+        RETURN;
+    END
+$$ LANGUAGE plpgsql;
+-- Question 8 : ok
+select * from preprojet.balance('563212564') t(date timestamp, tiers character(9), montant integer);
 
 -- QUESTION 9
-
+/*
+    Pour chaque compte en banque, ajoutez un champ balance_total. Ce champ contiendra la balance du compte en banque
+    (somme de tous les montants dont ce compte est destinataire moins la somme de tous les montants dont ce compte est l’origine).
+    Créez un trigger pour mettre ce champ à jour automatiquement.
+ */
 
 
 -- QUESTION 10
-
-
-
--- TESTS
-
--- Personnes
---select * from preprojet.personnes;
--- Comptes
---select * from preprojet.comptes;
--- Operations
---select * from preprojet.operations;
+/*
+    Pour chaque personne, ajoutez un champ balance_utilisateur. Ce champ contiendra la somme des balances de tous ses comptes.
+    Créez un trigger pour mettre ce champ à jour automatiquement.
+ */
