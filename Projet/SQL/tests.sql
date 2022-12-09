3a5e4e8c07ae79fe5cd1f7fdbb2a055e8a0f0f94
Jonathan CASIER <jonathancasier@gmail.com>
2022-12-10 00:45:10 +0100

    V1

--- Projet/SQL/tests.sql
+++ Projet/SQL/tests.sql
@@ -15,16 +15,19 @@ select * from projet.encoder_cours(CHARACTER(8) 'BINV1456', VARCHAR(20) 'cours 3
 
 -- encoder etudiant
 select * from projet.encoder_etudiant(VARCHAR(20) 'Damas', VARCHAR(20) 'Christophe',
-                             VARCHAR(50) 'christophe.damas@student.vinci.be', VARCHAR(20) 'mdp_cd');
+                             VARCHAR(50) 'cd@student.vinci.be', VARCHAR(20) '$2a$10$H/hwrpmkyTRwkAARnisq/uG91BvJuGNApOYqQSGjX3gAMPvMZ.jxm');
 select * from projet.encoder_etudiant(VARCHAR(20) 'Ferneeuw', VARCHAR(20) 'Stéphanie',
-                             VARCHAR(50) 'stephanie.ferneeuw@student.vinci.be', VARCHAR(20) 'mdp_sf');
+                             VARCHAR(50) 'sf@student.vinci.be', VARCHAR(20) '$2a$10$H/hwrpmkyTRwkAARnisq/uG91BvJuGNApOYqQSGjX3gAMPvMZ.jxm');
 select * from projet.encoder_etudiant(VARCHAR(20) 'Lehmann', VARCHAR(20) 'Brigitte',
-                             VARCHAR(50) 'brigitte.lehmann@student.vinci.be', VARCHAR(20) 'mdp_bl');
-select * from projet.etudiants;
+                             VARCHAR(50) 'bl@student.vinci.be', VARCHAR(20) '$2a$10$H/hwrpmkyTRwkAARnisq/uG91BvJuGNApOYqQSGjX3gAMPvMZ.jxm');
+select * from projet.encoder_etudiant(VARCHAR(20) 'Cambron', VARCHAR(20) 'Isabelle',
+                             VARCHAR(50) 'ic@student.vinci.be', VARCHAR(20) '$2a$10$H/hwrpmkyTRwkAARnisq/uG91BvJuGNApOYqQSGjX3gAMPvMZ.jxm');
+
 -- inscrire etudiant
-call projet.inscrire_etudiant(1, CHARACTER(8) 'BINV1345');
-call projet.inscrire_etudiant(2, CHARACTER(8) 'BINV1345');
-call projet.inscrire_etudiant(3, CHARACTER(8) 'BINV1345');
+call projet.inscrire_etudiant('cd@student.vinci.be', CHARACTER(8) 'BINV1345');
+call projet.inscrire_etudiant('sf@student.vinci.be', CHARACTER(8) 'BINV1345');
+call projet.inscrire_etudiant('bl@student.vinci.be', CHARACTER(8) 'BINV1345');
+call projet.inscrire_etudiant('ic@student.vinci.be', CHARACTER(8) 'BINV1345');
 
 -- creer projet
 select * from projet.creer_projet(VARCHAR(20) 'projSQL', CHARACTER(8) 'BINV1345', VARCHAR(20) '1p1',
@@ -41,27 +44,19 @@ select * from projet.inscription_groupe(1, 'projSQL', 1);
 select * from projet.inscription_groupe(2, 'projSQL', 1);
 select * from projet.inscription_groupe(3, 'projSQL', 2);
 
-select * from projet.visualiser_projets;
-
 -- valider groupe
 --call projet.valider_groupe('projSQL', 1);
 
 -- se retirer du groupe
 call projet.retirer_du_groupe(1, 3, VARCHAR(20) 'projSD');
-delete from projet.membres_groupe where etudiant = 1 and groupe = 1 and projet = 3;
-
-select g.est_valide from projet.groupes g where g.num = 1 and g.id_projet =1;
 
 select * from projet.groupes;
 select * from projet.membres_groupe;
-
 select * from projet.cours;
 select * from projet.etudiants;
 select * from projet.inscriptions_cours;
 select * from projet.projets;
-select * from projet.groupes;
-select * from projet.membres_groupe;
 
-select * from projet.visualiser_mes_projets_sans_groupes where "Etudiant" = 1;
+select * from projet.visualiser_groupes_incomplets(4, 'projSQL')
+    t(numero integer, nom varchar(20), prenom varchar(20), places integer, etudiant integer, identifiant varchar(20));
 
-select * from projet.visualiser_groupes_incomplets(1, 'projSD');
